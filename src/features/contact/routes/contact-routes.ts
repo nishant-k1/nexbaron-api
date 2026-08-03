a9caa621384f7f973095f4566e73d1a409526efb
Nishant Kumar <nishant.29k1@gmail.com>
2026-08-03 23:45:52 +0530

    Remove legacy tender and contact features with multer/uuid deps

--- src/features/contact/routes/contact-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { createContact } from '../controllers/contact-controller'
-import { validateContactRequest } from '../middleware/contact-validation'
-
-export const contactRouter = Router()
-
-contactRouter.post('/', validateContactRequest, createContact)
-
